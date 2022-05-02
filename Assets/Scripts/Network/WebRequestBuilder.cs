28eef48fed96ad91790bc1cdf26ac8ab411be290
Kamil Wąsowski <bazixx123@gmail.com>
2022-05-02 11:29:44 +0200

    Added Register and Login Form without connection to GameServer

---
+++ Assets/Scripts/Network/WebRequestBuilder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Networking;
+
+namespace Network
+{
+    public class WebRequestBuilder
+    {
+        private WebRequestBuilder()
+        {
+        }
+
+        private static WebRequestBuilder CreateInstance()
+        {
+            return new WebRequestBuilder();
+        }
+
+        private static WebRequestBuilder _instance;
+
+        public static WebRequestBuilder GetInstance()
+        {
+            if (_instance != null) return _instance;
+
+            _instance = CreateInstance();
+
+            return _instance;
+        }
+
+        public UnityWebRequest Request(string url, string methodType, byte[] requestBody)
+        {
+            var request = new UnityWebRequest(url, methodType)
+            {
+                uploadHandler = new UploadHandlerRaw(requestBody),
+                downloadHandler = new DownloadHandlerBuffer()
+            };
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            return request;
+        }
+    }
+}
