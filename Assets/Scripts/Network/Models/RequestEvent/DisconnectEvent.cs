c12ad51e8c2de55b5422a9ddda5187cdf591fd1b
Kamil Wąsowski <bazixx123@gmail.com>
2022-05-03 19:14:08 +0200

    Added Logout button

---
+++ Assets/Scripts/Network/Models/RequestEvent/DisconnectEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Network.Models.RequestEvent
+{
+    [Serializable]
+    public class DisconnectEvent: NetworkEvent
+    {
+        public DisconnectData data;
+    }
+
+    [Serializable]
+    public class DisconnectData
+    {
+        public string authorization;
+        public string jwtApi;
+        public string userId;
+    }
+}
